e052876d6357013f8302a39f105fe1a9cfbdbdeb
shiyangzhang <shiyangzhang@whale.im>
2022-06-20 13:57:47 +0800

    master:

--- src/whale/rancher.ts
+++ src/whale/rancher.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         demo-zsy
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://rancher-develop.meetwhale.com/p/c-t2g7l:p-gnhqj/workloads
